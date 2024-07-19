b7c7bb0601ffdaf1aea2d0dcf72042eac4023eaf
Русин Денис <denis.rusin2018@yandex.ru>
2024-07-19 09:38:39 +0300

    Груповой проект - добавлена функциональность Добавление режиссеров в фильмы 1.2

--- src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -7,13 +7,16 @@ import java.util.Set;
 
 public interface DirectorStorage {
     List<Director> getAllDirectors();
+
     Director getDirectorById(int id);
+
     Director addDirector(Director director);
+
     Director updateDirector(Director newDirector);
+
     void removeDirector(int directorId);
+
     Set<Director> getDirectorsFromFilm(int filmId);
-
-
 
 
 }
